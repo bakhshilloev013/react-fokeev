a6e93d6295aacee1a8c49ba9dd2fe8977ae79e8a
Bekhzod <beki2627@gmail.com>
2026-04-08 00:02:52 +0500

    weather app v2

--- practice/09-weather-wiget/src/App.jsx
+++ practice/09-weather-wiget/src/App.jsx
@@ -6,28 +6,35 @@ const API_KEY = 'b35a350d93be459c858160116260604';
 function App() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
 
   const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch(API_URL);
-      if (!res.ok) {
-        throw new Error(`status ${res.status}`);
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) {
+          throw new Error(
+            `Not valid city name ${res.status} ${res.statusText}`,
+          );
+        }
+        const json = await res.json();
+        setWeatherData(json);
+      } catch (error) {
+        setError(error.message);
       }
-      const json = await res.json();
-      setWeatherData(json);
     }
     fetchData();
   }, [city]);
-
-  console.log(weatherData);
 
   return (
     <div className="app">
       <div className="widget-container">
         <div className="weather-card-container">
-          <h1 className="app-title">Weather Widget</h1>
+          <h1 className="app-title">
+            {weatherData ? 'Weather Widget' : error}
+          </h1>
           <div className="search-container">
             <input
               value={city}
@@ -41,18 +48,18 @@ function App() {
         {weatherData && (
           <div className="weather-card">
             <h2>
-              {weatherData.location.country}, {weatherData.location.name}
+              {weatherData?.location?.country}, {weatherData?.location?.name}
             </h2>
             <img
-              src={weatherData.current.condition.icon}
+              src={weatherData?.current?.condition?.icon}
               alt="icon"
               className="weather-icon"
             />
-            <p className="temperature">{weatherData.current.temp_c}°C</p>
-            <p className="condition">{weatherData.current.condition.text}</p>
+            <p className="temperature">{weatherData?.current?.temp_c}°C</p>
+            <p className="condition">{weatherData?.current?.condition?.text}</p>
             <div className="weather-details">
-              <p>Humidity: {weatherData.current.humidity}%</p>
-              <p>Wind: {weatherData.current.wind_kph} km/h</p>
+              <p>Humidity: {weatherData?.current?.humidity}%</p>
+              <p>Wind: {weatherData?.current?.wind_kph} km/h</p>
             </div>
           </div>
         )}
